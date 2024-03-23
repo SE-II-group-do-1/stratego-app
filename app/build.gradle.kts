3105974a2c50e0dda0ee49811fd73e73fbf2735d
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-03-23 10:15:32 +0100

    ci/cd pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("org.sonarqube") version "4.4.1.3373"
+    jacoco
 }
 
 android {
@@ -31,13 +33,55 @@ android {
     }
 }
 
+
+tasks.withType<Test>().configureEach{
+    useJUnitPlatform()
+}
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDebugUnitTest")
+
+    reports {
+        xml.required = true
+        html.required = false
+        xml.getOutputLocation().set(file("${project.projectDir}/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"))
+    }
+
+    val fileFilter = mutableSetOf("**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*", "**/*Test*.*", "android/**/*.*")
+    val debugTree = fileTree("${project.layout.buildDirectory.get().asFile}/intermediates/javac/debug"){
+        exclude(fileFilter)
+    }
+    val mainSrc = "${project.projectDir}/src/main/java"
+
+    sourceDirectories.setFrom(mainSrc)
+    classDirectories.setFrom(debugTree)
+    executionData.setFrom("${project.layout.buildDirectory.get().asFile}/jacoco/testDebugUnitTest.exec")
+}
+
+tasks.named("jacocoTestReport") {
+    finalizedBy(this)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SE-II-group-do-1_stratego-app")
+        property("sonar.organization", "se-ii-group-do-1")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.projectDir}/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+    }
+}
+
 dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("org.jacoco:org.jacoco.core:0.8.5")
     testImplementation(libs.junit)
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
