6ae29103d3a8e0590441cd62982cda3c12d1b0d8
Katharina Kortschak <kakortschak@edu.aau.at>
2024-03-26 18:22:53 +0100

    feat: set up initial app structure with fragment-based navigation
    - Created MainActivity as the primary entry point of the app.
    - Added FragmentContainerView in MainActivity layout for fragment management.
    - Implemented MainFragment, GameFragment, and SettingsFragment for main app functionalities.

---
+++ app/src/main/java/com/example/stratego_app/GameFragment.java
@@ -0,0 +1,41 @@
+package com.example.stratego_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.function.ToDoubleBiFunction;
+
+
+public class GameFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        /*ToDo: read about how to use fragments*/
+        //Information for me: Inflating the XML file is how you create this View object from a static XML file.
+        //needed when working with fragments
+        return inflater.inflate(R.layout.fragment_game_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // look up GameBoardView by ID - GameBoardView not implemented yet
+        /*ToDo: implement GamBoardView*/
+        GameBoardView gameBoardView = view.findViewById(R.id.gameBoardView);
+         /*
+        place to add interaction modules for board game.
+        Nature of implementation has not been decided on yet.
+         */
+    }
+
+}
---
+++ app/src/main/java/com/example/stratego_app/SettingsFragment.java
@@ -0,0 +1,36 @@
+package com.example.stratego_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        /*ToDo: implement GamBoardView*/
+        //GameBoardView is also used in GameFragment
+
+        GameBoardView gameBoardView = view.findViewById(R.id.settingsGameBoardView);
+        //gameBoardView.setConfigurationMode(true);
+
+        /*
+        place to add setup for board game. Nature of implementation has not been decided on.
+         */
+
+    }
+
+}
