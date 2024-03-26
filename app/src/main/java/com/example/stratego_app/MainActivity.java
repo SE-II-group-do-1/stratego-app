7a64e9f6ae8ecfdbd96e12d4b56e424814f17644
Katharina Kortschak <kakortschak@edu.aau.at>
2024-03-26 18:22:53 +0100

    feat: set up initial app structure with fragment-based navigation
    - Created MainActivity as the primary entry point of the app.
    - Added FragmentContainerView in MainActivity layout for fragment management.
    - Implemented MainFragment, GameFragment, and SettingsFragment for main app functionalities.

--- app/src/main/java/com/example/stratego_app/MainActivity.java
+++ app/src/main/java/com/example/stratego_app/MainActivity.java
@@ -13,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        /*Container so Main fragment = start Page can be displayed and executed*/
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new MainFragement())
+                    .commit();
+        }
     }
 }
