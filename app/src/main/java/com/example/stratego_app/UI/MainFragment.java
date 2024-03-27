2a87b3af536cc155e9e8a22f1e613291624f7c78
Katharina Kortschak <kakortschak@edu.aau.at>
2024-03-27 12:57:39 +0100

    refactor(ui): organize relevant UI classes in UI package

---
+++ app/src/main/java/com/example/stratego_app/UI/MainFragment.java
@@ -0,0 +1,46 @@
+package com.example.stratego_app.UI;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.stratego_app.R;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button settingsButton = view.findViewById(R.id.settings);
+        settingsButton.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new SettingsFragment());
+            fragmentTransaction.addToBackStack(null);//makes transaction reversible "back" button in app
+            fragmentTransaction.commit();
+        });
+
+        Button startGame = view.findViewById(R.id.enterGame);
+        startGame.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new GameFragment());
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
+
+    }
+}
