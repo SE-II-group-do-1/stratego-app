c63e086c0ea48e6792558c7c0672d725c2020a6a
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-05-05 13:14:57 +0200

    fix: corrected build script, path to LobbyClient

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ sonar {
         property("sonar.coverage.jacoco.xmlReportPaths", "${project.projectDir}/build/reports/jacoco/test/jacocoTestReport.xml")
         // Exclude UI tests from SonarQube analysis
         property("sonar.coverage.exclusions", "src/main/java/com/example/stratego_app/ui/**," +
-                "src/main/java/com/example/stratego_app/ui/MockSessionService.java," + "src/main/java/com/example/stratego_app/connection/clients/LobbyClient.java," + "src/main/java/com/example/stratego_app/models/Player.java")
+                "src/main/java/com/example/stratego_app/ui/MockSessionService.java," + "src/main/java/com/example/stratego_app/connection/LobbyClient.java," + "src/main/java/com/example/stratego_app/models/Player.java")
     }
 }
 
