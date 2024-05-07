b3d4e7f6b4e54d5851d1942079af8bc58324fdef
Katharina Kortschak <kakortschak@edu.aau.at>
2024-05-07 18:31:40 +0200

    test: add tests class player

---
+++ app/src/test/java/com/example/stratego_app/PlayerTest.java
@@ -0,0 +1,46 @@
+package com.example.stratego_app;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.stratego_app.model.Player;
+
+class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("testUsername");
+    }
+
+    @Test
+    void testPlayerInitializationWithUsername() {
+        assertEquals("testUsername", player.getUsername());
+    }
+
+    @Test
+    void testSetUsername() {
+        player.setUsername("newUsername");
+        assertEquals("newUsername", player.getUsername());
+    }
+
+    @Test
+    void testGetIdBeforeSet() {
+        assertEquals(0, player.getId());
+    }
+
+    @Test
+    void testSetId() {
+        player.setId(1234);
+        assertEquals(1234, player.getId());
+    }
+
+    @Test
+    void testPlayerToString() {
+        player.setId(1);
+        String expectedString = "Player{username='testUsername', id=1}";
+        assertEquals(expectedString, player.toString());
+    }
+}
+
