781ce383c25caef4141f6d30c2e40a80923579b1
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-05-08 18:42:28 +0200

    fix: piece tests and piece add visible method

--- app/src/main/java/com/example/stratego_app/model/Piece.java
+++ app/src/main/java/com/example/stratego_app/model/Piece.java
@@ -52,4 +52,8 @@ public class Piece {
     public int getId() {
         return id;
     }
+
+    public void setVisible(boolean b){
+        this.isVisible = b;
+    }
 }
--- app/src/test/java/com/example/stratego_app/PieceTest.java
+++ app/src/test/java/com/example/stratego_app/PieceTest.java
@@ -19,7 +19,7 @@ import com.example.stratego_app.model.Rank;
 class PieceTest {
     @Test
     public void testPieceInitialization() {
-        Piece soldier = new Piece(Rank.SERGEANT, Color.BLUE, 1);
+        Piece soldier = new Piece(Rank.SERGEANT, Color.BLUE);
         assertEquals("Rank should be SERGEANT", Rank.SERGEANT, soldier.getRank());
         assertEquals("Color should be BLUE", Color.BLUE, soldier.getColor());
         assertEquals("ID should be 1", 1, soldier.getId());
@@ -37,7 +37,7 @@ class PieceTest {
 
     @Test
     public void testVisibilityToggle() {
-        Piece flag = new Piece(Rank.FLAG, Color.RED, 2);
+        Piece flag = new Piece(Rank.FLAG, Color.RED);
         assertFalse("Visibility should initially be false", flag.isVisible());
         flag.setVisible(true);
         assertTrue("Visibility should be toggleable to true", flag.isVisible());
@@ -46,8 +46,8 @@ class PieceTest {
     @Test
     public void testMovability() {
         // Checking all non-movable types
-        Piece bomb = new Piece(Rank.BOMB, Color.RED, 3);
-        Piece flag = new Piece(Rank.FLAG, Color.BLUE, 4);
+        Piece bomb = new Piece(Rank.BOMB, Color.RED);
+        Piece flag = new Piece(Rank.FLAG, Color.BLUE);
         Piece lake = new Piece(Rank.LAKE);
 
         assertFalse("Bombs should not be movable", bomb.isMovable());
@@ -55,7 +55,7 @@ class PieceTest {
         assertFalse("Lakes should not be movable", lake.isMovable());
 
         // Checking a movable type
-        Piece captain = new Piece(Rank.CAPTAIN, Color.BLUE, 5);
+        Piece captain = new Piece(Rank.CAPTAIN, Color.BLUE);
         assertTrue("Captains should be movable", captain.isMovable());
     }
 }
