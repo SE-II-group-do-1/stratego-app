fedbe470dbd8b5abef1abcd3fe5f221cfabd9c61
klaus-maria <63562466+klaus-maria@users.noreply.github.com>
2024-06-16 13:30:26 +0200

    feat: added winner property to updatemessage

--- app/src/main/java/com/example/stratego_app/connection/UpdateMessage.java
+++ app/src/main/java/com/example/stratego_app/connection/UpdateMessage.java
@@ -2,6 +2,7 @@ package com.example.stratego_app.connection;
 
 
 import com.example.stratego_app.model.Board;
+import com.example.stratego_app.model.Player;
 
 public class UpdateMessage {
     public Board getBoard() {
@@ -27,10 +28,17 @@ public class UpdateMessage {
     public void setLobbyID(int lobbyID) {
         this.lobbyID = lobbyID;
     }
+    public void setWinner(Player winner){
+        this.winner = winner;
+    }
+    public Player getWinner() {
+        return winner;
+    }
 
     private Board board;
     private int initiator;
     private int lobbyID;
+    private Player winner;
 
 }
 
